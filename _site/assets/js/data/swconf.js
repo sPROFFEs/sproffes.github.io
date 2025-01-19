1ad66acab7c7b9524aacbbf00ea0871b13cfc340
pr0ff3 <150958256+sPROFFEs@users.noreply.github.com>
2025-01-19 13:07:02 +0100

    testing

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737242922',resources: [
+    cacheName: 'chirpy-1737283099',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
