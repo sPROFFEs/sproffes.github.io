e734100993578586088206557c918a3146fdff7a
pr0ff3 <150958256+sPROFFEs@users.noreply.github.com>
2025-01-19 15:28:35 +0100

    new content

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737283099',resources: [
+    cacheName: 'chirpy-1737296841',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
